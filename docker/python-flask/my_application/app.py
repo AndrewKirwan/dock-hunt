cbeb197c56011bfe880a5025f4c2ed2313f3db20
Spook <adasfsae>
2014-10-30 15:43:48 +0000

    Added Euler code

--- docker/python-flask/my_application/app.py
+++ docker/python-flask/my_application/app.py
@@ -13,6 +13,26 @@ def listfiles():
 	filelist = [f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 	return(filelist)
 
+@app.route("/Euler1")
+def Euler1():
+  finalsum = 0
+  for i in range(1000):
+    if i % 3 == 0:
+      finalsum += i
+    elif i % 5 == 0:
+      finalsum += i
+  return(finalsum)
+
+@app.route("/Euler2")
+def Euler2():
+  x, y = 1, 1
+  finalsum = 0
+  while x <= 4000000:
+    if x % 2 == 0:
+      finalsum += x
+    x, y = y, x+y
+  print(finalsum)
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
