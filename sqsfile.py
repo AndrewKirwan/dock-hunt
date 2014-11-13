356ce5cc31cd709693694fb4b4a0f8cce296055a
Andrew Kirwan <stormspud@live.ie>
2014-11-13 15:25:17 +0000

    Update sqsfile.py

--- sqsfile.py
+++ sqsfile.py
@@ -4,7 +4,7 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region('us-east-1',aws_access_key_id='AKIAIR7EH3TNSTDUCWKA',aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region('us-east-1',aws_access_key_id='BKIAIR7EH3TNSTDUCWKB',aws_secret_access_key='s2FZT5mrLYy8gX7kS1q0p4ObQYXTwGnaiUm+rxHA')
 
 q=conn.create_queue('C12339291')
 
